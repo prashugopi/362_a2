93fd4a98a0f6421744261bfb618df2efc2cb84bf
Prashanth Gopinatth <prashugopi@gmail.com>
2021-11-09 22:58:57 -0800

    added functionality to check for lower case letters

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,13 @@
 def check_pwd(password):
+    lower = 0
+
     if len(password) < 8 or len(password) > 20:
         return False
+    
+    for i in password:
+        if i.islower():
+            lower += 1
+    if lower == 0:
+        return False
+
     return True
